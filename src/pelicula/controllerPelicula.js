0a29e99e5f2e873ddaf17444d266af0ad6b03509
GonzaloVillagra <Gonzalo.villagra.f@gmail.com>
2024-09-20 16:25:14 -0300

    Update controllerPelicula.js

--- src/pelicula/controllerPelicula.js
+++ src/pelicula/controllerPelicula.js
@@ -3,7 +3,7 @@ import client from "../common/db.js";
 import { Pelicula } from "./pelicula.js";
 
 
-const peliculaCollection = client.db('Pelicula'.collection('Peliculas'))
+const peliculaCollection = client.db('pelicula'.collection('Peliculas'))
 
 //Funcion Agregar Pelicula
 async function handleInserPeliculaRequest(req, res) {

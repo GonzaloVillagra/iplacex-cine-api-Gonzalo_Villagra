a372481e6fe670c714ad36133547d1f580f4509e
GonzaloVillagra <Gonzalo.villagra.f@gmail.com>
2024-09-20 16:42:00 -0300

    Update index.js

--- index.js
+++ index.js
@@ -13,24 +13,25 @@ app.use(express.json());
 app.use(urlencoded({ extended: true}));
 app.use(cors());
 
-app.all('/', (req, res) => {return res.status(200).send('bienvenido al control de gustos')})
-app.use('/api',  routes)
+import express from 'express';
+import routes from './routes'; 
+import client from './common/db.js'; 
 
-await client.connect()
-.then(() =>{
-    console.log('conectado al cluster')
-    app.listen(PORT, () => {console.log(`servidor esta corriendo en http://localhost:${PORT}`);
-        
-    });
-
-})
-.catch(() => {
-    console.log('Ha ocurrido un error al conectar')
-
-})
-
+app.use('/api', routes);
 
 app.get('/', (req, res) => {
-    let message = "bienvenido a la Api Rest para el control de las peliculas";
+    const message = "Bienvenido a la API REST para el control de las películas";
     return res.status(200).send(message);
 });
+
+await client.connect()
+    .then(() => {
+        console.log('Conectado al cluster');
+        app.listen(PORT, () => {
+            console.log(`Servidor está corriendo en http://localhost:${PORT}`);
+        });
+    })
+    .catch((error) => {
+        console.log('Ha ocurrido un error al conectar:', error);
+    });
+

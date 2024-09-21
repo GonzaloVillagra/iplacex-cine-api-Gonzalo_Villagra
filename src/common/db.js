e9ea1b0c9f89df4a343d77ad2fd70069f6906a0a
GonzaloVillagra <Gonzalo.villagra.f@gmail.com>
2024-09-21 13:10:12 -0300

    Update db.js

--- src/common/db.js
+++ src/common/db.js
@@ -3,13 +3,15 @@ import { MongoClient, ServerApiVersion } from "mongodb";
 const uri = 'mongodb+srv://ev3_express:qt2yc3VyNaeQyGkZ@cluster-express.fybpz.mongodb.net/pelicula?retryWrites=true&w=majority';
 
 const client = new MongoClient(uri, {
-    serverApi:{
-    version: ServerApiVersion.v1,
-    strict: true,
-    deprecationErrors: true
-        }
-    }
-);
+    serverApi: {
+        version: ServerApiVersion.v1,
+        strict: true,
+        deprecationErrors: true
+    },
+    tls: true,  // Asegura que la conexión use TLS
+    tlsAllowInvalidCertificates: false,  // Rechaza certificados inválidos (si es el caso)
+});
+
 
 export default client
 
